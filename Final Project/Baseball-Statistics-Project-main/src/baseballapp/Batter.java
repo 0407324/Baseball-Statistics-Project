3d3deb4ab8443d1359829be97c7b94b90f216d5a
its.Magzzz <70177478+0407324@users.noreply.github.com>
2021-04-30 10:39:36 -0500

    Add files via upload

--- Final Project/Baseball-Statistics-Project-main/src/baseballapp/Batter.java
+++ Final Project/Baseball-Statistics-Project-main/src/baseballapp/Batter.java
@@ -1,8 +1,52 @@
-
 package baseballapp;
 
-public class Batter {
-   // TODO: create the attr of the Batter class i.e. fName, lName, position..
-   // You will also need to create the getter and setter methods for this application. 
-   // 
+public class Batter 
+{
+    private String lastName;
+    private String firstName;
+    private String position;
+    private int ab;
+    private int r; 
+    private int h; 
+    private int rbi; 
+    private int bb; 
+    private int so; 
+    private int po; 
+    private int a; 
+    private int lob; 
+
+    public Batter(){
+        this("","","",0,0,0,0,0,0,0,0,0);
+    }
+
+
+    public Batter(String lastName,String firstName,String position,int ab,int r,int h,int rbi,int bb, int so, int po, int a, int lob){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.position = position;
+        this.ab = ab;
+        this.r = r;
+        this.h = h;
+        this.rbi = rbi;
+        this.bb = bb; 
+        this.so = so;
+        this.po = po;
+        this.a = a;
+        this.lob = lob;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public String getPosition()
+    {
+        return position;
+    }
+
 }
